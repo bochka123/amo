b028aef4c7e2146e8e04fc74e0e209cb6d2ee7b0
bochka123 <sasha2003tkach001100@gmail.com>
2022-02-16 20:53:15 +0200

    begin

--- docs/jsForLab1.js
+++ docs/jsForLab1.js
@@ -1,25 +1,76 @@
 $(document).ready(()=>{
     $("#linearButton").click(()=>{
-        let a = $("#linearInput1").val()
-        let b = $("#linearInput2").val()
-        let c = $("#linearInput3").val()
+        let a = Math.round($("#linearInput1").val())
+        let b = Math.round($("#linearInput2").val())
+        let c = Math.round($("#linearInput3").val())
         if(parseInt(a)&&parseInt(b)&&parseInt(c)){
-            let Y1 = (Math.sqrt(a+b)/(c+2*b)) + (Math.sin(a)/Math.sqrt(Math.pow(a, 2)-Math.pow(b, 2)))
+            let Y1 = (Math.sqrt(a+b)/(c+2*b)) + (Math.sin(a)/Math.sqrt(a**2-b**2))
+            Y1 = Math.round(Y1*100)/100
             if(!isNaN(Y1)){
                 if(isFinite(Y1)) {
-                    $("#answer").html(`Відповідь: ${Y1}`)
+                    $("#answer1").html(`Відповідь: ${Y1}`)
                 }
                 else {
-                    $("#answer").html("Вийшло ділення на нуль. Спробуйте знову")
+                    $("#answer1").html("Вийшло ділення на нуль. Спробуйте знову")
                 }
             }else {
-                $("#answer").html("Вийшов корінь із від'ємного числа. Спробуйте знову")
+                $("#answer1").html("Вийшов корінь із від'ємного числа. Спробуйте знову")
             }
         }else{
-            $("#answer").html("Ви ввели не число(")
+            $("#answer1").html("Ви ввели не число(")
         }
         $("#linearInput1").val("")
         $("#linearInput2").val("")
         $("#linearInput3").val("")
     })
+    $("#ramifiedButton").click(()=>{
+        let n = Math.round($("#ramifiedInput1").val())
+        let r = Math.round($("#ramifiedInput2").val())
+        if(parseInt(r)&&parseInt(n)) {
+            if(n >= 0 && r >= 0 && n-r>0) {
+                let condition = factorial(n) / factorial(r) * factorial(n - r)
+                let y
+                if (condition > 2345) {
+                    y = factorial(n) / factorial(n - r)
+                } else {
+                    y = n ** r / factorial(n - r)
+                }
+                $("#answer2").html(`Відповідь: ${y}`)
+            }else {
+                $("#answer2").html("Факторіал може бути лише з додатнього числа(")
+            }
+        }else {
+            $("#answer2").html("Ви ввели не число(")
+        }
+        $("#ramifiedInput1").val("")
+        $("#ramifiedInput2").val("")
+    })
+    $("#loopButton").click(()=>{
+        let sum = 0
+        let product = 1
+        for (let i = 1; i <= 5; i++){
+            sum += i**3
+        }
+        for (let i = 1; i <= 5; i++){
+            product *= i**6
+        }
+        let f = sum/product
+
+        $("#answer3").html(`Відповідь: ${sum} / ${product} = ${f}`)
+    })
 })
+function factorial(num){
+    if (num < 0) {
+        console.error("Variable num can't be negative")
+        $("#answer2").html("Факторіал може бути лише з додатнього числа")
+        return 0
+    }else if(num === 0){
+        return 1
+    }else{
+        let answer = 1
+        for (let i = 1; i <= num; i++) {
+            answer *= i
+        }
+        return answer
+    }
+}
